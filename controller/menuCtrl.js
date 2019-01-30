7099de3847259da1bef6f9327dedd4c46fe8ab1a
sonal <sonal.kesare@syslogic.com>
2019-01-30 11:15:22 +0530

    changes in menus

--- controller/menuCtrl.js
+++ controller/menuCtrl.js
@@ -12,13 +12,12 @@ app.controller("menuCtrl", function ($scope, services, $http, $location, $cookie
     if(loggedInUser.identity.role==RESOURCES.ROLE_ADMIN){
         $scope.menuList = [
             {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"},
+            {"Title": "Client Management", "Link": "/company", "icon": "fa fa-bank ", "active":"deactive"},
             {"Title": "JD Management", "Link": "/jobs", "icon": "fa fa-tasks", "active":"deactive"},
-            {"Title": "Company Management", "Link": "/company", "icon": "fa fa-bank ", "active":"deactive"},
-            {"Title": "Resume Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"},
+            {"Title": "Candidate Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"},
+            {"Title": "Screening", "Link": "/questions", "icon": "fa fa-question-circle", "active":"deactive"},
+            {"Title": "Scheduled Interview", "Link": "/interview_list", "icon": "fa fa-calendar", "active":"deactive"},
             {"Title": "Forward Resumes", "Link": "/forward_resumes", "icon": "fa fa-circle-o", "active":"deactive"},
-            // {"Title": "Round Details", "Link": "/round_details", "icon": "fa fa-info", "active":"deactive"},
-            {"Title": "Screening Questions", "Link": "/questions", "icon": "fa fa-question-circle", "active":"deactive"},
-            {"Title": "Scheduled interview", "Link": "/interview_list", "icon": "fa fa-calendar", "active":"deactive"},
             {"Title": "Background Checklist", "Link": "/background_checklist", "icon": "fa fa-check-square-o", "active":"deactive"},
             {"Title": "User Management", "Link": "user#all", "icon": "fa fa-user-plus", "active":"deactive"}
         ];
@@ -26,33 +25,39 @@ app.controller("menuCtrl", function ($scope, services, $http, $location, $cookie
     }else if (loggedInUser.identity.role==RESOURCES.ROLE_HR) {
         $scope.menuList = [
             {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
-            {"Title": "Resume Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"},
-            {"Title": "User Management", "Link": "user#all", "icon": "fa fa-user-plus", "active":"deactive"}
+            {"Title": "Candidate Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"},
+            {"Title": "Screening", "Link": "/questions", "icon": "fa fa-question-circle", "active":"deactive"},
+            {"Title": "Scheduled Interview", "Link": "/interview_list", "icon": "fa fa-calendar", "active":"deactive"},
+            {"Title": "Forward Resumes", "Link": "/forward_resumes", "icon": "fa fa-circle-o", "active":"deactive"},
+            {"Title": "Background Checklist", "Link": "/background_checklist", "icon": "fa fa-check-square-o", "active":"deactive"},
+            // {"Title": "User Management", "Link": "user#all", "icon": "fa fa-user-plus", "active":"deactive"}
         ];      
         $scope.showUpdateProfile = true;
     }else if (loggedInUser.identity.role==RESOURCES.ROLE_COLLEGUE) {
         $scope.menuList = [
             {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
-            {"Title": "Resume Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"}
+            {"Title": "Candidate Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"}
         ];
         $scope.showUpdateProfile = true;
     }else if (loggedInUser.identity.role==RESOURCES.ROLE_INTERVIWER) {
         $scope.menuList = [
-            {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
-            {"Title": "Scheduled interview", "Link": "/interview_list", "icon": "fa fa-calendar", "active":"deactive"},
-            {"Title": "Today's interviews", "Link": "/today_interviews", "icon": "fa fa-calendar", "active":"deactive"}
+            // {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
+            {"Title": "Today's interviews", "Link": "/today_interviews", "icon": "fa fa-calendar", "active":"active"},
+            {"Title": "Scheduled interview", "Link": "/interview_list", "icon": "fa fa-calendar", "active":"deactive"}
         ];
         $scope.showUpdateProfile = true;
     }else if (loggedInUser.identity.role==RESOURCES.ROLE_CANDIDATE) {
         $scope.menuList = [
-            {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
-            {"Title": "My Resume", "Link": "/view_resume", "icon": "fa fa-file-text", "active":"deactive"},
-            {"Title": "Upload Documents", "Link": "/upload_background_form", "icon": "fa fa-file", "active":"deactive"}
+            // {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}, 
+            {"Title": "My Resume", "Link": "/view_resume", "icon": "fa fa-file-text", "active":"active"},
+            {"Title": "Documents", "Link": "/upload_background_form", "icon": "fa fa-file", "active":"deactive"}
         ];
         $scope.showUpdateProfile = false;
     }else if (loggedInUser.identity.role==RESOURCES.ROLE_CLIENT) {
         $scope.menuList = [
-            {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"}
+            {"Title": "Dashboard", "Link": "/home", "icon": "fa fa-dashboard", "active":"active"},
+            {"Title": "JD Management", "Link": "/jobs", "icon": "fa fa-tasks", "active":"deactive"},
+            {"Title": "Candidate Management", "Link": "/resume_list#non-selected", "icon": "fa fa-file-text", "active":"deactive"}
         ];
         $scope.showUpdateProfile = false;
     }else{
